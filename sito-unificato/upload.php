27e58df7efba7047ea5651cfed051d495836ae79
android <mabu32@hotmail.com>
2026-06-04 16:26:52 +0200

    Cambiato oggetto mail in Documentazione per rinnovo Patente

--- sito-unificato/upload.php
+++ sito-unificato/upload.php
@@ -38,7 +38,7 @@ if (isset($_FILES['foto']) && $_FILES['foto']['error'] === UPLOAD_ERR_OK) {
 $id = uniqid('doc-');
 
 $to = 'info@trybox.it';
-$subject = 'Nuova documentazione ricevuta - ID #' . $id;
+$subject = 'Documentazione per rinnovo Patente';
 
 $boundary = md5(time());
 
